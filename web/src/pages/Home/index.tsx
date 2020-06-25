4bd31e6c08463ff3c05e9e7b5605643be2577425
victorhugoeliziani <victorhugoeliziani@gmail.com>
2020-06-25 19:39:34 -0300

    Inserindo o background da página e inicio da construção do menu

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,38 +1,37 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 import '../../assets/css/bootstrap.min.css';
+import '../../assets/css/styles.css'; //CSS GERAL
 import './styles.css';
 
 const Home = () => {
     return (
         <>
-            <div className="navbar-nav navbar-inverse navbar-fixed-top">
+            <header>
                 <div className="container">
-                    <div className="navbar-header">
-                        <a className="navbar-brand">V-FOOD</a>
-                    </div>
-                    <div className="navbar-collapse collapse">
-                        <ul className="nav navbar-nav">
-                            <li className="active">
-                                <a href="/">HOME</a>
-                            </li>
-                            <li>
-                                <a href="/create-categories">CATEGORIAS</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-sm-3 col-lg-3">
-                        <div className="dash-unit">
-                            <span>Perfil do usuário</span>
-                            <hr/>
-                        </div>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <nav className="menu">
+                                <ul>
+                                    <li>
+                                        <Link to="/create-categories">Categorias</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/create-categories">Categorias</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/create-categories">Categorias</Link>
+                                    </li>
+                                    <li>
+                                        <Link to="/create-categories">Categorias</Link>
+                                    </li>
+                                </ul>
+                            </nav>    
+                        </div>                        
                     </div>
                 </div>
-            </div>
+            </header>
         </>
     );
 }
