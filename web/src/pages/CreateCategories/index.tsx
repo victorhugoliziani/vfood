33a8d1350667c004947d02ce9f35c41ba299211f
victorhugoeliziani <victorhugoeliziani@gmail.com>
2020-06-28 11:10:18 -0300

    Estilização da página de cadastro de categorias

--- web/src/pages/CreateCategories/index.tsx
+++ web/src/pages/CreateCategories/index.tsx
@@ -13,31 +13,33 @@ const CreateCategories = () => {
                     <div className="col-md-12">
                         <div className="card">
                             <div className="card-header">
-                                Cadastro de categorias
+                                <h1>Cadastro de categorias</h1>
                             </div>
                             <div className="card-body">
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        <label htmlFor="name">Nome</label>
-                                        <input type="text" name="name" id="name" className="name"/>
+                                <form className="formCategories" id="formCategories">
+                                    <div className="row">
+                                        <div className="col-md-12">
+                                            <label htmlFor="name">Nome</label>
+                                            <input type="text" name="name" id="name" className="name"/>
+                                        </div>
+                                        <div className="col-md-12">
+                                            <label htmlFor="description">Description</label>
+                                            <input type="textarea" name="description" id="description" className="description"/>
+                                        </div>
+                                        <div className="col-md-12">
+                                            <label htmlFor="parent_id">Sub-categoria</label>
+                                            <select name="parent_id" className="parent_id" id="parent_id">
+                                                <option>Selecione uma opção</option>
+                                            </select>
+                                        </div>
+                                        <div className="col-md-12">
+                                            <Dropzone />
+                                        </div>
+                                        <div className="col-md-12">
+                                            <button type="button" className="btnSave" name="btnSave" id="btnSave">Cadastrar</button>
+                                        </div>
                                     </div>
-                                    <div className="col-md-12">
-                                        <label htmlFor="description">Description</label>
-                                        <input type="textarea" name="description" id="description" className="description"/>
-                                    </div>
-                                    <div className="col-md-12">
-                                        <label htmlFor="parent_id">Sub-categoria</label>
-                                        <select name="parent_id" className="parent_id" id="parent_id">
-                                            <option>Selecione uma opção</option>
-                                        </select>
-                                    </div>
-                                    <div className="col-md-12">
-                                        <Dropzone />
-                                    </div>
-                                    <div className="col-md-12">
-                                        <button type="button" className="btnSave" name="btnSave" id="btnSave">Salvar</button>
-                                    </div>
-                                </div>
+                                </form>
                             </div>
                         </div>
                     </div>
